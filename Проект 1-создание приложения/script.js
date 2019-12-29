576c8385b01e17d185bbe42b7cf84c4e2e8c67bb
Konstantin <konstantingoncharuk50@gmail.com>
2019-12-29 14:45:34 +0300

    Урок 7-Получение элементов со страницы

--- Проект 1-создание приложения/script.js
+++ Проект 1-создание приложения/script.js
@@ -18,7 +18,6 @@ var appData = {
     optionalExpences: {},
     income: [],
     timeData: time,
-<<<<<<< HEAD
     savings: true,
     chooseExpences: function() {
         for(var i = 0; i<2; i++) {
@@ -87,66 +86,3 @@ for (var key in appData) {
 }
 
 
-=======
-    savings: true
-};
-
-function chooseExpences() {
-    for(var i = 0; i<2; i++) {
-        var a = prompt('Введите обязательную статью разходов в этом месяце', ''),
-            b = prompt('Во сколько обойдется?', '');
-        if ((typeof(a)==='string') && (typeof(a))!= null && (typeof(b))!= null && a != '' && b != '' && a.length < 50) {
-            console.log("done");
-            appData.expences[a] = b;
-        }   else {
-            console.log ("bad result");
-            i = i - 1;
-        }
-    }
-}
-
-chooseExpences();
-
-function detectDayBudget() {
-    appData.moneyPerDay = (appData.budget / 30).toFixed();
-    alert('Ежедневный бюджет: ' + appData.moneyPerDay);
-}
-
-detectDayBudget();
-
-function detectLevel() {
-    if (appData.moneyPerDay < 100) {
-        console.log("минимальный уровень достатка");
-    }   else if (appData.moneyPerDay > 100 && appData.moneyPerDay < 2000) {
-        console.log("средний уровень достатка");
-    }   else if (appData.moneyPerDay > 2000) {
-        console.log("Высокий уровень достатка");
-    }   else {
-        console.log("error");
-    }
-}
-
-detectLevel();
-
-function checkSavings () {
-    if (appData.savings == true) {
-        var save = +prompt('Какова сумма накоплений?', ''),
-            percent = +prompt('Под какой процент?', '');
-
-            appData.monthIncome = save/100/12*percent;
-            alert('Доход в месяц с вашего депозита: ' + appData.monthIncome);
-    }
-}
-
-checkSavings();
-
-function chooseOptExpenses() {
-    for (var i = 1; i <= 3; i++) {
-        var questionOptExpenses = prompt("Статья необязательных расходов?");
-        appData.optionalExpenses[i] = questionOptExpenses;
-        console.log(optionalExpences);
-    }
-}
-
-chooseOptExpenses();
->>>>>>> master
